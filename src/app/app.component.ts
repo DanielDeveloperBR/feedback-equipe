088eecc9faf0a270ab540820dfeceb924d64c32e
Daniel S <danieldetrabalho@hotmail.com>
2025-01-29 01:03:58 -0300

    primeiro commit, fiz a pagina home, e agora falta a do login e coloquei uns estilos no root e usando bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'projetoFeedback';
 }
