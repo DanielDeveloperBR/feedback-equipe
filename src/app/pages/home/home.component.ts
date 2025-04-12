c555b6429e44d65a102307ed63512121e2227d9d
Daniel S <danieldetrabalho@hotmail.com>
2025-04-11 21:13:27 -0300

    sem alterações

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  imports: [HeaderComponent, FooterComponent]
+  imports: [HeaderComponent, FooterComponent, CommonModule]
 })
 export class HomeComponent {
+  usersList: any[] = []
+  constructor(public users: AuthService) { }
 
+  ngOnInit(){
+  this.mostrarUsers()
+}
+
+mostrarUsers() {
+  this.users.mostrarUsers().subscribe({
+    next: (data: any) => {
+      this.usersList = data.sucess
+    },
+    error: (err) => {
+      console.error("Erro ao buscar usuários:", err);
+    }
+  });
+}
+
+  
 }
