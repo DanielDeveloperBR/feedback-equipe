9eda70326560bb6f331cad49644283d203b2b71e
Daniel S <danieldetrabalho@hotmail.com>
2025-05-29 17:33:48 -0300

    removi o css todo e coloquei diretamente com o tailwind e umas modificações; no html mudei as rotas que estaam e coloquei para ter menu de hamb; no ts import o common e o meddo do menu

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-side-bar',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css'
 })
 export class SideBarComponent {
+  menuOpen = false;
   constructor(private router: Router, private authService: AuthService){}
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
   logout(): void {
     this.authService.logout();
     this.router.navigate(['/login']); 
