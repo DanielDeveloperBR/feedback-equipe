3bceeb26d935ad31b0dfb294c8d6416009fd4c71
Daniel S <danieldetrabalho@hotmail.com>
2025-05-29 17:31:08 -0300

    coloquei a nova rota do dashboard que carrega diretamente no componente do dashboard e os seus filhos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,19 +2,16 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CadastrarComponent } from './pages/cadastrar/cadastrar.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { AuthGuard } from './guards/auth.guard';
 import { RedirectGuard } from './guards/redirect.guard';
-import { FeedbacksComponent } from './pages/feedbacks/feedbacks.component';
 import { TermosDeUsoComponent } from './pages/termos-de-uso/termos-de-uso.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     // Verifica se está logado antes de ir para Home
-    { path: '', component: HomeComponent, canActivate: [RedirectGuard] }, 
+    { path: '', component: HomeComponent, canActivate: [RedirectGuard] },
     { path: 'login', component: LoginComponent, canActivate: [RedirectGuard] },
     { path: 'cadastro', component: CadastrarComponent, canActivate: [RedirectGuard] },
     { path: 'termos-de-uso', component: TermosDeUsoComponent, canActivate: [RedirectGuard] },
-    { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-    { path: 'feedbacks', component: FeedbacksComponent, canActivate: [AuthGuard] },
-    { path: '**', redirectTo: '', pathMatch: 'full' }
+    {path: 'dashboard',loadChildren: () => import('./pages/dashboard/dashboard.routes').then(m => m.dashboardRoutes), canActivate: [AuthGuard]},
+{ path: '**', redirectTo: '', pathMatch: 'full' }
 ];
