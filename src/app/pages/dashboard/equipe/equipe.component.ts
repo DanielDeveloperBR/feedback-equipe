624156e7742653717482311574877a99ba53fe00
Daniel S <danieldetrabalho@hotmail.com>
2025-08-26 14:29:20 -0300

    coloquei para ilustrar como poderia ser a página de equipe e no ts colocando um array para mostrar

--- src/app/pages/dashboard/equipe/equipe.component.ts
+++ src/app/pages/dashboard/equipe/equipe.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-equipe',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './equipe.component.html',
   styleUrl: './equipe.component.css'
 })
 export class EquipeComponent {
+  funcionarios = [
+    {nome: 'Alice', email: 'alicecu@gmail.com', cargo: 'Desenvolvedora Front-end', telefone: '(11) 91234-5678'},
+    {nome: 'Bruno', email: 'brunobizonho@gmail.com', cargo: 'Desenvolvedor Back-end', telefone: '(11) 92345-6789'},
+    {nome: 'Paulo', email: 'paulo@gmail.com', cargo: 'Designer UX/UI', telefone: '(11) 93456-7890'},
+  ];
 
 }
